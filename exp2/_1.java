a5ef2df1be0f6862fbf0253c0ef1ae3928688230
Aditya Yadav <ishqaddy@gmail.com>
2023-07-31 20:08:50 +0530

    fix tabsize

--- exp2/_1.java
+++ exp2/_1.java
@@ -7,27 +7,27 @@
 import java.util.Scanner;
 
 public class _1 {
-    public static void main(String args[]) {
-        Scanner s = new Scanner(System.in);
-        System.out.printf("enter marks in physics: ");
-        int physics = s.nextInt();
-        System.out.printf("enter marks in chemistry: ");
-        int chemistry = s.nextInt();
-        System.out.printf("enter marks in maths: ");
-        int maths = s.nextInt();
-        s.close();
-        int pc = (physics + chemistry + maths) / 3;
-        // marks checking condition's
-        if (pc >= 75) {
-            System.out.println("distinction");
-        } else if (pc >= 60 && pc < 75) {
-            System.out.println("first class");
-        } else if (pc >= 50 && pc < 60) {
-            System.out.println("second class");
-        } else if (pc >= 40 && pc < 50) {
-            System.out.println("pass");
-        } else if (pc < 40) {
-            System.out.println("fail");
-        }
+  public static void main(String args[]) {
+    Scanner s = new Scanner(System.in);
+    System.out.printf("enter marks in physics: ");
+    int physics = s.nextInt();
+    System.out.printf("enter marks in chemistry: ");
+    int chemistry = s.nextInt();
+    System.out.printf("enter marks in maths: ");
+    int maths = s.nextInt();
+    s.close();
+    int pc = (physics + chemistry + maths) / 3;
+    // marks checking condition's
+    if (pc >= 75) {
+      System.out.println("distinction");
+    } else if (pc >= 60 && pc < 75) {
+      System.out.println("first class");
+    } else if (pc >= 50 && pc < 60) {
+      System.out.println("second class");
+    } else if (pc >= 40 && pc < 50) {
+      System.out.println("pass");
+    } else if (pc < 40) {
+      System.out.println("fail");
     }
+  }
 }
--- exp2/_3.java
+++ exp2/_3.java
@@ -7,15 +7,15 @@
 import java.util.Scanner;
 
 public class _3 {
-    public static void main(String args[]) {
-        Scanner s = new Scanner(System.in);
-        System.out.printf("enter number: ");
-        int number = s.nextInt();
-        int i = 1;
-        s.close();
-        while (i <= 20) {
-            System.out.printf("%d * %d = %d\n", number, i, number * i);
-            i++;
-        }
+  public static void main(String args[]) {
+    Scanner s = new Scanner(System.in);
+    System.out.printf("enter number: ");
+    int number = s.nextInt();
+    int i = 1;
+    s.close();
+    while (i <= 20) {
+      System.out.printf("%d * %d = %d\n", number, i, number * i);
+      i++;
     }
+  }
 }
